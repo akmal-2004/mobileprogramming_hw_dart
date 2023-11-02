337a306e691de900d7d84cb0e32491f5ea35a1eb
akmal-2004 <akmallatifov2004@gmail.com>
2023-11-02 16:04:14 +0500

    problem 3

---
+++ first 12 problems/p3.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('even numbers:');
+  for (var i = 2; i <= 100; i += 2) {
+    print(i);
+  }
+  
+  print('odd numbers:');
+  for (var i = 1; i <= 100; i += 2) {
+    print(i);
+  }
+}
